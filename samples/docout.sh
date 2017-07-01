01431fe83a89ffec12ef4d023535357c1138b45b
Valentyn Shybanov <olostan@gmail.com>
2017-07-01 08:07:22 +0300

    Built web

---
+++ samples/docout.sh
@@ -0,0 +1,30 @@
+flutter doctor
+[✓] Flutter (on Mac OS X 10.12.5 16F73, locale en-UA, channel master)
+    • Flutter at /Users/vshybanov/code/dart/flutter/flutter
+    • Framework revision 1438ae85a3 (3 hours ago), 2017-06-30 16:09:48 -0700
+    • Engine revision 8ad1576f27
+    • Tools Dart version 1.24.2
+
+[✓] Android toolchain - develop for Android devices (Android SDK 25.0.3)
+    • Android SDK at /usr/local/opt/android-sdk
+    • Platform android-25, build-tools 25.0.3
+    • ANDROID_HOME = /usr/local/opt/android-sdk
+    • Java binary at: /Applications/Android Studio.app/Contents/jre/jdk/Contents/Home/bin/java
+    • Java version OpenJDK Runtime Environment (build 1.8.0_112-release-b06)
+
+[✓] iOS toolchain - develop for iOS devices (Xcode 8.3.3)
+    • Xcode at /Applications/Xcode.app/Contents/Developer
+    • Xcode 8.3.3, Build version 8E3004b, 
+    • ios-deploy 1.9.0
+    • CocoaPods version 1.2.1
+
+[✓] Android Studio (version 2.3)
+    • Android Studio at /Applications/Android Studio.app/Contents
+    • Java version OpenJDK Runtime Environment (build 1.8.0_112-release-b06)
+
+[✓] IntelliJ IDEA Community Edition (version 2017.1.4)
+    • Flutter plugin version 15.0
+    • Dart plugin version 171.4694.29
+
+[✓] Connected devices
+    • None
---
+++ samples/install.sh
@@ -0,0 +1,2 @@
+$ git clone -b alpha https://github.com/flutter/flutter.git
+$ export PATH=`pwd`/flutter/bin:$PATH
